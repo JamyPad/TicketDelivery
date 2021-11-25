2b838f6ef0d34b360b9aeee9bce731eacac9dcc0
JamyPad <injm.pad@gmail.com>
2021-11-25 04:04:57 -0600

    Add files via upload
    An app developed to keep track of Christmas goodies ticket delivery to Caracol Knits employees in collaboration with 3 other trainees.

---
+++ Dominio/Entidades/Entregas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Entregas
+    {
+        public string Codigo { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Departamento { get; set; }
+        public string CodigoDepartamento { get; set; }
+        public string Empresa { get; set; }
+        public string Estado { get; set; }
+        public DateTime? FechaEntrega { get; set; }
+    }
+}
